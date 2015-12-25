fe75cbbdf021c6802bbd32ba866fc688b17f49b7
fanna <fanna993@gmail.com>
2015-12-25 17:25:43 +0100

    Add random number generator and animation

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<time.h>
+#include <stdlib.h>
 
 typedef struct {
     char id_len;
@@ -39,29 +41,8 @@ void create_header(HEADER h, FILE *tga)
 
 void color(int r, int g, int b, int a, FILE *tga);
 
-void draw(int x, int y, int width, int height, int r, int g, int b, int a)
+void draw_rect(int x, int y, int width, int height, int r, int g, int b, int a, FILE *tga)
 {
-  FILE *tga;
-
-  HEADER header;
-
-  header.id_len = 0;
-  header.map_type = 0;
-  header.img_type = 2;
-  header.map_first = 0;
-  header.map_len = 0;
-  header.map_entry_size = 0;
-  header.x = 0;
-  header.y = 0;
-  header.width = 640;
-  header.height = 480;
-  header.bpp = 32;
-  header.desc= 0x20;
-
-  tga = fopen("picture.tga", "w+");
-
-  create_header(header, tga);
-
   int paint_x;
   int paint_y;
   for(paint_x = 0; paint_x < 480; paint_x++)
@@ -76,13 +57,62 @@ void draw(int x, int y, int width, int height, int r, int g, int b, int a)
         color(255, 255, 255, 0, tga);
       }
     }
+}
 
-  fclose(tga);
+int random_param()
+{
+  int random_num = ( rand() % 256 ) + 1;
+
+  printf("%d\n", random_num);
+
+  return random_num;
+}
+
+void animate()
+{
+  int frame_index;
+
+  for(frame_index = 0; frame_index < 23; frame_index++)
+  {
+    FILE *tga;
+
+    int x = random_param();
+    int y = random_param();
+    int height  = random_param();
+    int width  = random_param();
+    int r = random_param();
+    int g = random_param();
+    int b = random_param();
+
+    HEADER header;
+
+    header.id_len = 0;
+    header.map_type = 0;
+    header.img_type = 2;
+    header.map_first = 0;
+    header.map_len = 0;
+    header.map_entry_size = 0;
+    header.x = 0;
+    header.y = 0;
+    header.width = 640;
+    header.height = 480;
+    header.bpp = 32;
+    header.desc= 0x20;
+
+    char filename[64];
+    sprintf (filename, "anim%d.tga", frame_index);
+    tga = fopen(filename, "wb");
+
+    create_header(header, tga);
+    draw_rect(x, y, width, height, r, g, b, 255, tga);
+
+    fclose(tga);
+  }
 }
 
 int main ()
 {
-  draw(100, 240, 160, 130, 134, 255, 111, 255);
+  animate();
 
   return 0;
 }
